55cbcb72674962999cda3fcd21729876b840b4cc
Jack Kallend <Kallendo96@live.co.uk>
2020-07-16 17:50:31 +0100

    Added auth routes and exercise schema

---
+++ models/Exercise.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const ExerciseSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+module.exports = mongoose.model('User', ExerciseSchema)
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 // @desc    Login
 // @route   GET /
-router.get('/', (req, res) => {
+router.get('/', ensureGuest, (req, res) => {
   res.render('login', { layout: 'login' })
 })
 
 // @desc    Dashboard
 // @route   GET /
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard')
+router.get('/dashboard', ensureAuth, async (req, res) => {
+  try {
+    const exercises = await Exercise.find({ user: req.user.id }).lean()
+
+    res.render('dashboard', {
+      name: req.user.firstName,
+      exercises,
+    })
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 module.exports = router
